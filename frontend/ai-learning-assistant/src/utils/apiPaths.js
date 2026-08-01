34d1f1a91c3ddab5b553108ceaa6ad6fa504e329
sanehakaur77 <snehaswali9988@gmail.com>
2026-08-01 20:09:58 +0530

    saneha

--- frontend/ai-learning-assistant/src/utils/apiPaths.js
+++ frontend/ai-learning-assistant/src/utils/apiPaths.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "https://ai-learning-assistant-app.onrender.com";
+export const BASE_URL = "https://ai-interview-prep-web-app-bi5d.onrender.com";
 
 export const API_PATHS = {
   AUTH: {
