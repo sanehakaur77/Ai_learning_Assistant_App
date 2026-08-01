7bc5faea20eb2ab62e5452685de5fa3d8c393e6f
sanehakaur77 <snehaswali9988@gmail.com>
2026-08-01 20:46:21 +0530

    s

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,5 @@
-import dotenv from "dotenv";
+import dotenv from 'dotenv'
 dotenv.config();
-import mongoose from "mongoose";
 import express from "express";
 import cors from "cors";
 import path from "path";
